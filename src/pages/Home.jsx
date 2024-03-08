807fb833edd7ba64d3b7e6f607a7bee8c87d4af1
danil volobuyev <dani736@gmail.com>
2024-03-08 17:36:29 +0200

    feat: added static island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import Loader from "../components/Loader";
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Loader from '../components/Loader';
+import Island from '../models/Island';
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -9,13 +10,41 @@ import Loader from "../components/Loader";
 }
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, rotation] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={rotation}
+          />
+        </Suspense>
       </Canvas>
     </section>
   );
